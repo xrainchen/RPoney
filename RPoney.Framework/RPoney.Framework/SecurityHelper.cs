5286f0d6bfc10f561d3830dcd55932f552246883
xrainchen <xrainchen@163.com>
2017-02-07 14:03:10 +0800

    安全算法策略模式

--- RPoney.Framework/RPoney.Framework/SecurityHelper.cs
+++ RPoney.Framework/RPoney.Framework/SecurityHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.Cryptography;
+using RPoney.Security;
+using RPoney.Security.Imp;
 
 namespace RPoney
 {
@@ -7,7 +9,14 @@ namespace RPoney
     /// </summary>
     public static class SecurityHelper
     {
-
+        /// <summary>
+        /// 安全服务工厂
+        /// </summary>
+        static ISecurityServiceFactory _securityServiceFactory;
+        static SecurityHelper()
+        {
+            _securityServiceFactory = new SecurityServiceFactory();
+        }
         /// <summary>
         /// 加密成MD5哈希字节数组
         /// </summary>
@@ -15,9 +24,26 @@ namespace RPoney
         /// <returns>MD5加密的哈希字节数组</returns>
         public static byte[] EncryMd5Bytes(this byte[] input)
         {
-            return new MD5CryptoServiceProvider().ComputeHash(input);
+            return _securityServiceFactory.CreateSecurityService(SecurityServiceEnum.MD5).EncryToBytes(input);
         }
-
+        /// <summary>
+        /// 加密成MD5哈希字节数组
+        /// </summary>
+        /// <param name="input">源字节数组</param>
+        /// <returns>MD5加密的16进制字符串</returns>
+        public static string EncryMd5String(this byte[] input)
+        {
+            return input.EncryMd5Bytes().GetHexString();
+        }
+        /// <summary>
+        /// 加密成MD5哈希字节数组
+        /// </summary>
+        /// <param name="input">源字节数组</param>
+        /// <returns>MD5加密的16进制字符串</returns>
+        public static string EncryLowerMd5String(this byte[] input)
+        {
+            return input.EncryMd5Bytes().GetLowerHexString();
+        }
         /// <summary>
         /// 加密成SHA1哈希字节数组
         /// </summary>
@@ -25,7 +51,7 @@ namespace RPoney
         /// <returns>SHA1加密的哈希字节数组</returns>
         public static byte[] EncrySha1Bytes(this byte[] input)
         {
-            return new SHA1CryptoServiceProvider().ComputeHash(input);
+            return _securityServiceFactory.CreateSecurityService(SecurityServiceEnum.SHA1).EncryToBytes(input);
         }
         /// <summary>
         /// 加密成SHA1哈希字节数组
@@ -34,7 +60,16 @@ namespace RPoney
         /// <returns>SHA1加密16进制字符串</returns>
         public static string EncrySha1String(this byte[] input)
         {
-            return new SHA1CryptoServiceProvider().ComputeHash(input).GetHexString();
+            return input.EncrySha1Bytes().GetHexString();
+        }
+        /// <summary>
+        /// 加密成MD5哈希字节数组
+        /// </summary>
+        /// <param name="input">源字节数组</param>
+        /// <returns>SHA1加密16进制字符串</returns>
+        public static string EncryLowerSha1String(this byte[] input)
+        {
+            return input.EncrySha1Bytes().GetLowerHexString();
         }
     }
 }
