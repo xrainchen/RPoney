5151fd13cf838f9c58367d98aeed50585a2ca303
xrainchen <xrainchen@qq.com>
2017-08-19 12:00:06 +0800

    修改log配置方式

--- RPoney.Log.Test/Program.cs
+++ RPoney.Log.Test/Program.cs
@@ -9,8 +9,16 @@ namespace RPoney.Log.Test
             RPoney.Log.LoggerManager.Debug("Program", "Debug1");
             RPoney.Log.LoggerManager.Info("Program", "Info1");
             RPoney.Log.LoggerManager.Warn("Program", "Warn1");
-            RPoney.Log.LoggerManager.Error("Program", "Error1");
+            try
+            {
+                var a = int.Parse("134f");
+            }
+            catch (Exception ex)
+            {
+                RPoney.Log.LoggerManager.Error("Program", "Error1",ex);
+            }
             RPoney.Log.LoggerManager.Fatal("Program", "Fatal1");
+            Console.WriteLine("记录完成");
             Console.Read();
         }
     }
